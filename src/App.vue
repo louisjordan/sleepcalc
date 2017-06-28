2250ec548a8332b8fbc63c5ebe372a8f279f75d2
Louis Jordan <louisjordan@hotmail.com>
2017-06-28 21:10:00 +0100

    Moved repo

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <div class="page-content">
+      <sleep-calc class="sleep-calc"></sleep-calc>
+    </div>
+  </div>
+</template>
+
+<script>
+import SleepCalc from './SleepCalc.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return {}
+  },
+  components: {
+    'sleep-calc': SleepCalc
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  height: 100%;
+  font-family: Helvetica, sans-serif;
+  font-weight: 200;
+  font-size: 1.3em;
+}
+
+body {
+  animation: gradientAnimation 100s alternate infinite;
+  background-size: 750% 750%;
+  background-color: #304b80;
+  background-image: -moz-linear-gradient(-45deg, #304b80, #80304b, #4b8030, #80304b, #304b80);
+  background-image: -webkit-gradient(-45deg, #304b80, #80304b, #4b8030, #80304b, #304b80);
+  background-image: -webkit-linear-gradient(-45deg, #304b80, #80304b, #4b8030, #80304b, #304b80);
+  background-image: -o-linear-gradient(-45deg, #304b80, #80304b, #4b8030, #80304b, #304b80);
+  background-image: -ms-linear-gradient(-45deg, #304b80, #80304b, #4b8030, #80304b, #304b80);
+  background-image: linear-gradient(-45deg, #304b80, #80304b, #4b8030, #80304b, #304b80);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#304b80', endColorstr='#4b8030', GradientType=1);
+}
+
+@keyframes gradientAnimation {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 100% 100%;
+  }
+}
+
+input,
+button {
+  font-family: Helvetica, sans-serif;
+  font-weight: 200;
+}
+
+.page-content {
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.sleep-calc {
+  width: 100%;
+  max-width: 400px;
+  min-height: 350px;
+  margin: 5vw;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/SleepCalc.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="container">
+  
+    <p>
+      <button @click="mode = mode === 'wakeupat' ? 'fallasleepat' : 'wakeupat'">Toggle</button>
+    </p>
+  
+    <p>
+      If I want to
+      <b v-if="mode === 'wakeupat'">wake up</b>
+      <b v-else>fall asleep</b>
+      at
+    </p>
+  
+    <input v-model="hours" type="number" max="24" min="-1"> :
+    <input v-model="minutes" type="number" max="60" min="-15" step="15">
+    <button v-show="mode === 'fallasleepat'" @click="setTime">Now</button>
+  
+    <p>
+      I should
+      <b v-if="mode === 'wakeupat'">fall asleep</b>
+      <b v-else>wake up</b>
+      at
+    </p>
+  
+    <ul>
+      <li v-for="(time, index) in times" :key="index">{{ time }}</li>
+    </ul>
+  
+  </div>
+</template>
+
+<script>
+const formatTime = function (time) {
+  time = new Date(time);
+
+  let hours = time.getHours();
+  hours = hours > 9 ? hours : `0${hours}`;
+
+  let minutes = time.getMinutes();
+  minutes = minutes > 9 ? minutes : `0${minutes}`;
+
+  return `${hours}:${minutes}`
+};
+
+const calculateWakeIntervals = function (time, forward = true) {
+  let intervals = [];
+
+  for (var i = 1; i <= 7; i++) {
+    let interval = 1000 * 60 * 90 * i;
+    intervals.push(formatTime(forward ? time + interval : time - interval));
+  }
+
+  return forward ? intervals.reverse() : intervals;
+};
+
+const padLeft = function (input, maxLength, padCharacter) {
+  input = String(input);
+  const paddingLength = maxLength - input.length;
+  const padding = new Array(paddingLength).fill(padCharacter);
+  return padding.join('') + input;
+}
+
+export default {
+  name: 'sleep-calc',
+  data() {
+    return {
+      mode: 'wakeupat',
+      hours: '08',
+      minutes: '00',
+      time: ''
+    }
+  },
+  updated() {
+    const hours = Number(this.hours);
+    const minutes = Number(this.minutes);
+
+    if (minutes === -15) {
+      this.minutes = 45;
+      this.hours = hours - 1;
+    } else if (minutes === 60) {
+      this.minutes = 0;
+      this.hours = hours + 1;
+    }
+
+    if (this.hours == -1) {
+      this.hours = 23;
+    } else if (this.hours == 24) {
+      this.hours = 0;
+    }
+
+    this.hours = padLeft(this.hours, 2, '0');
+    this.minutes = padLeft(this.minutes, 2, '0');
+  },
+  computed: {
+    times() {
+      const currentTime = new Date(1970, 0, 1, this.hours, this.minutes).getTime();
+      return calculateWakeIntervals(currentTime, this.mode === 'fallasleepat');
+    }
+  },
+  methods: {
+    setTime() {
+      const now = formatTime(Date.now()).split(':');
+      this.hours = now[0];
+      this.minutes = now[1];
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  text-align: center;
+  border: 1px solid white;
+  padding: 32px;
+  color: white;
+  opacity: 0.8;
+}
+
+input {
+  color: white;
+  background: none;
+  text-align: center;
+  border: 0;
+  outline: 0;
+  border-bottom: 2px solid white;
+}
+
+button {
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+  color: white;
+  border: 0;
+  outline: 0;
+  padding: 5px 10px;
+  transition: 0.3s ease;
+  cursor: pointer;
+}
+
+button:hover {
+  background: rgba(0, 0, 0, 0.4);
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 8px;
+  background: rgba(0, 0, 0, 0.2);
+  padding: 8px 16px;
+  border-radius: 3px;
+}
+</style>
